60a794a55a6b2fe2fd3fe0d4e15821639a1cf208
bhavi321 <bhavialkhaniya5@gmail.com>
2023-02-17 02:09:59 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import { useEffect, useState } from 'react';
+import {useState} from "react"
 import './App.css';
 
-
-const Card = ({id,title,body})=>{
-  return (
-  <div className = "container">
-    <strong>id:</strong>{id} <br/>
-    <strong>title:</strong> {title} <br/>
-    <strong>body:</strong>{body}
-  </div>
-  )
-}
-
-
 function App() {
-  const [posts,setPosts] = useState([])
-  
-
-  useEffect(()=>{
- fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>response.json()).then((data)=>setPosts(data))
-  },[])
-
+const [state,setState]=useState(0)
   return (
     <div className="App">
-    {posts && posts.length>0 && posts.map((postsObj,index)=>{
-     return (<Card key = {postsObj.id} 
-          id={postsObj.id}
-          title={postsObj.title}
-          body={postsObj.body}
-    
-
-    />)})}
-
+      Count:{state}<br/>
+      <button onClick={()=>setState(state+1)}>Increase</button><br/>
+      <button onClick={()=>setState(state-1)}>Decrease</button>
     </div>
-  )
+  );
 }
 
 export default App;
